df9ffe99e4be48018d93d319bdffdc13cc37d568
Sam Churchill <meadowschurchill@gmail.com>
2016-06-01 14:56:05 -0700

    directives and stuff

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
 (function(){
-	var app = angular.module('store', [ ]);
+	var app = angular.module('example', [ ]);
 	
-	app.controller('SphereController', function(){
-		this.current = sphere;
+	app.controller('ExampleController', function(){
+		this.spheres = triangulations;
 	});
 	
-	var sphere = {
-		name: 'Tetrahedron',
-		euler: 2,
-		faces: 4,
-		edges: 6,
-		vertices: 4,
-		description: "Boundary of the 3-simplex."
-	}
+	var triangulations = [
+		{
+			name: 'Tetrahedron',
+			euler: 2,
+			faces: 4,
+			edges: 6,
+			vertices: 4,
+			description: "Boundary of the 3-simplex.",
+			isExample: true,
+			isComputable: false,
+		},
+		{
+			name: 'Octohedron',
+			euler: 2,
+			faces: 8,
+			edges: 12,
+			vertices: 6,
+			description: "Dual to the cube.",
+			isExample: false,
+			isComputable: true,
+		},
+	]
 })();

0d449d55e34b1e80008433154e9ef1ba6a61427d
Sam Churchill <meadowschurchill@gmail.com>
2016-06-01 14:16:38 -0700

    Some substance to the angular

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('store', [ ]);
+(function(){
+	var app = angular.module('store', [ ]);
+	
+	app.controller('SphereController', function(){
+		this.current = sphere;
+	});
+	
+	var sphere = {
+		name: 'Tetrahedron',
+		euler: 2,
+		faces: 4,
+		edges: 6,
+		vertices: 4,
+		description: "Boundary of the 3-simplex."
+	}
+})();

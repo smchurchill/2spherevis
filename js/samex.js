16fc8613e9011c14093549c54f8a92b7ec3452de
Sam Churchill <meadowschurchill@gmail.com>
2016-06-06 21:40:05 -0700

    link

--- js/samex.js
+++ js/samex.js
@@ -5,21 +5,46 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var geometry = new THREE.Geometry();
+
 var points = [
-    new THREE.Vector3(-0.915225,1.115363,-0.497874),
-    new THREE.Vector3(1.083538,0.903080,-0.583045),
-    new THREE.Vector3(0.326438,-1.379865,-0.564766),
-    new THREE.Vector3(-0.884288,-1.066278,0.640807),
-    new THREE.Vector3(-1.418295,0.492836,0.274045),
+    new THREE.Vector3(0.736087,1.274940,0.402807),
     new THREE.Vector3(0.000000,0.000000,-1.526286),
     new THREE.Vector3(1.472174,0.000000,0.402807),
     new THREE.Vector3(-1.420338,0.253597,-0.497873),
-    new THREE.Vector3(0.453966,-0.949486,1.105414),
+    new THREE.Vector3(0.453966,y:-0.949486,z:1.105414),
+    new THREE.Vector3(-0.915225,1.115363,-0.497874),
+    new THREE.Vector3(1.083538,0.903080,-0.583045),
+    new THREE.Vector3(-0.326438,-1.379865,-0.564766),
+    new THREE.Vector3(-0.884288,-1.066278,0.640807),
+    new THREE.Vector3(-1.418295,0.492836,0.274045),
     new THREE.Vector3(0.596459,-1.404884,-0.009311),
-    new THREE.Vector3(1.154489,0.998308,0.009254),
-    new THREE.Vector3(0.736087,1.274940,0.402807)
+    new THREE.Vector3(1.154489,0.998308,0.009254)
+];    
+
+var faces = [
+    new THREE.Face3(0,1,2),
+    new THREE.Face3(3,1,2),
+    new THREE.Face3(0,4,2),
+    new THREE.Face3(0,1,5),
+    new THREE.Face3(3,4,2),
+    new THREE.Face3(3,1,6),
+    new THREE.Face3(0,4,5),
+    new THREE.Face3(7,1,5),
+    new THREE.Face3(3,4,8),
+    new THREE.Face3(7,1,6),
+    new THREE.Face3(3,9,6),
+    new THREE.Face3(10,4,5),
+    new THREE.Face3(7,11,5),
+    new THREE.Face3(10,4,8),
+    new THREE.Face3(3,9,8),
+    new THREE.Face3(7,9,6),
+    new THREE.Face3(10,11,5),
+    new THREE.Face3(7,11,8),
+    new THREE.Face3(10,11,8),
+    new THREE.Face3(7,9,8)
 ];
-var geometry = new THREE.ConvexGeometry(points);
+
 var material = new THREE.MeshBasicMaterial({color:0x00ff00});
 var sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);

173948bdecd4d879608a6db5df7240ac98c6a8a5
Sam Churchill <meadowschurchill@gmail.com>
2016-06-06 21:11:06 -0700

    use convexgeometry

--- js/samex.js
+++ js/samex.js
@@ -5,8 +5,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.Geometry();
-geometry.vertices.push(
+var points = [
     new THREE.Vector3(-0.915225,1.115363,-0.497874),
     new THREE.Vector3(1.083538,0.903080,-0.583045),
     new THREE.Vector3(0.326438,-1.379865,-0.564766),
@@ -19,8 +18,8 @@ geometry.vertices.push(
     new THREE.Vector3(0.596459,-1.404884,-0.009311),
     new THREE.Vector3(1.154489,0.998308,0.009254),
     new THREE.Vector3(0.736087,1.274940,0.402807)
-);
-
+];
+var geometry = new THREE.ConvexGeometry(points);
 var material = new THREE.MeshBasicMaterial({color:0x00ff00});
 var sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);

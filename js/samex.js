59f788467cdd384412f5a18646c2e96bf01aff44
Sam Churchill <meadowschurchill@gmail.com>
2016-06-06 20:49:23 -0700

    three

--- js/samex.js
+++ js/samex.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 50);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.webGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);

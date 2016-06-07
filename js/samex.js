4b6d2b1e10cd36d91ef6492b05500532d83ccf97
Sam Churchill <meadowschurchill@gmail.com>
2016-06-06 21:00:01 -0700

    sphere?

--- js/samex.js
+++ js/samex.js
@@ -5,10 +5,26 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1,1,1);
+var geometry = new THREE.Geometry();
+points = [
+    new THREE.Vector3(-0.915225,1.115363,-0.497874),
+    new THREE.Vector3(1.083538,y:0.903080,z:-0.583045),
+    new THREE.Vector3(0.326438,y:-1.379865,z:-0.564766),
+    new THREE.Vector3(-0.884288,y:-1.066278,z:0.640807),
+    new THREE.Vector3(-1.418295,y:0.492836,z:0.274045),
+    new THREE.Vector3(0.000000,y:0.000000,z:-1.526286),
+    new THREE.Vector3(1.472174,y:0.000000,z:0.402807),
+    new THREE.Vector3(-1.420338,y:0.253597,z:-0.497873),
+    new THREE.Vector3(0.453966,y:-0.949486,z:1.105414),
+    new THREE.Vector3(0.596459,y:-1.404884,z:-0.009311),
+    new THREE.Vector3(1.154489,y:0.998308,z:0.009254),
+    new THREE.Vector3(0.736087,y:1.274940,z:0.402807)
+];
+geometry.vertices.push(points);
+
 var material = new THREE.MeshBasicMaterial({color:0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 camera.position.z = 5;
 

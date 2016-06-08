cb71e44b5cd734938fc905ba4897c4c268d9d074
Sam Churchill <meadowschurchill@gmail.com>
2016-06-07 21:37:26 -0700

    var knowledge

--- js/utils.js
+++ js/utils.js
@@ -48,25 +48,23 @@ function addTexturedGeometry(file, geometry, scene) {
 
 function init(container, stats, scene, camera, renderer) {
     
-    container = document.createElement('div');
+    var container = document.createElement('div');
     document.body.appendChild(container);
     
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
     camera.position.y = 400;
-    
-    scene = new THREE.Scene();                    
+                     
     scene.add(new THREE.AmbientLight(0x404040));
     
     var light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0,1,0);
     scene.add(light);
 
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    var renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
     
-    stats = new Stats();
+    var stats = new Stats();
     container.appendChild(stats.dom);
     
     window.addEventListener('resize', onWindowResize, false);

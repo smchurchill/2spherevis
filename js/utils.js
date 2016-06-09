7033c3ea6355002cc9e9de4c42693fdfedd1452b
Sam Churchill <meadowschurchill@gmail.com>
2016-06-08 19:00:27 -0700

    hmhmhm

--- js/utils.js
+++ js/utils.js
@@ -160,10 +160,10 @@ function init() {
 
     //CAMERA
     var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-    var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 2000;
+    var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 10000;
     camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
     scene.add(camera);
-    camera.position.set(0,150,2*radius);
+    camera.position.set(0,150,4*radius);
     camera.lookAt(scene.position);
     
     //RENDERER
@@ -193,7 +193,7 @@ function init() {
     light.position.set(100,250,100);
     scene.add(light);
     
-    var skyBoxGeometry = new THREE.CubeGeometry(1000,1000,1000);
+    var skyBoxGeometry = new THREE.CubeGeometry(FAR*0.5,FAR*0.5,FAR*0.5);
     var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x6666cc, side: THREE.BackSide } );
     var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
     scene.add(skyBox);
